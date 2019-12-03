845ed3e9d2033dcccbd64770d80af5f3b474c14a
Brandon Kammerdiener <kammerdienerb@gmail.com>
2019-12-03 12:43:05 -0800

    updates after hmalloc profiling fix

--- benchmarks/test/test.c
+++ benchmarks/test/test.c
@@ -4,20 +4,22 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
+#define MAIN
+#define THREADS
+
 void* thr(void *_ptr) {
     int *ptr;
     int  i, j;
 
-    printf("THREAD tid      = %d\n", syscall(SYS_gettid));
+    printf("THREAD tid      = %ld\n", syscall(SYS_gettid));
     free(malloc(2000));
 
     ptr = _ptr;
 
-    for (i = 0; i < 50; i += 1) {
-        for (j = 0; j < 100000; j += 1) {
+    for (i = 0; i < 5000; i += 1) {
+        for (j = 2000000; j < 3000000; j += 1) {
             ptr[j] = j;
         }
-        sleep(1);
     }
 
     return NULL;
@@ -30,28 +32,32 @@ int main() {
     pthread_t  t[20];
 
     printf("pid             = %d\n", getpid());
-    printf("main thread tid = %d\n", syscall(SYS_gettid));
+    printf("main thread tid = %ld\n", syscall(SYS_gettid));
 
-    ptr = malloc(100000 * sizeof(int));
+    ptr = malloc(3000000 * sizeof(int));
 
+#ifdef THREADS
+    for (i = 0; i < 20; i += 1) {
+        if (pthread_create(t + i, NULL, thr, ptr) != 0) {
+            printf("wut\n");
+            return 1;
+        }
+    }
+#endif
 
-/*     for (i = 0; i < 20; i += 1) { */
-/*         if (pthread_create(t + i, NULL, thr, ptr) != 0) { */
-/*             printf("wut\n"); */
-/*             return 1; */
-/*         } */
-/*     } */
-
-    for (i = 0; i < 50; i += 1) {
-        for (j = 0; j < 100000; j += 1) {
+#ifdef MAIN
+    for (i = 0; i < 5000; i += 1) {
+        for (j = 2000000; j < 3000000; j += 1) {
             ptr[j] = j;
         }
-        sleep(1);
     }
+#endif
 
-/*     for (i = 0; i < 20; i += 1) { */
-/*         pthread_join(t[i], &junk); */
-/*     } */
+#ifdef THREADS
+    for (i = 0; i < 20; i += 1) {
+        pthread_join(t[i], &junk);
+    }
+#endif
 
     free(ptr);
 
