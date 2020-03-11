c34dc72090b9d09df73e5050815cafc205bbdaa2
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-03-11 05:52:56 -0700

    fix lulesh inputs

--- benchmarks/lulesh/bench_run.sh
+++ benchmarks/lulesh/bench_run.sh
@@ -1,8 +1,6 @@
-ITERS=5
-
-input_small=" -s 220 -i $ITERS -r 11 -b 0 -c 64 -p"
-input_medium="-s 340 -i $ITERS -r 11 -b 0 -c 64 -p"
-input_large=" -s 420 -i $ITERS -r 11 -b 0 -c 64 -p"
+input_small=" -s 220 -i 12 -r 11 -b 0 -c 64 -p"
+input_medium="-s 520 -i 6 -r 11 -b 0 -c 64 -p"
+input_large=" -s 690 -i 3 -r 11 -b 0 -c 64 -p"
 
 function do_bench_run {
     run_prepared_cmd
